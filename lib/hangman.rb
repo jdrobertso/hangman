135f3a2eb9122f7a9b0d32de665f93be24bd7bf3
jdrobertso <jrobertson.88@gmail.com>
2017-09-04 19:08:02 -0500

    Added preliminary logic

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,3 +1,8 @@
-
-
-dictionary = File.read "lib/5desk.txt"
+def get_word
+  dictionary = File.readlines "lib/5desk.txt"
+  wordindex = Random.new.rand(0..10000)
+  word = dictionary[wordindex]
+  return word unless word.length < 5 && word.length > 12
+end
+target = get_word
+turns = 5
